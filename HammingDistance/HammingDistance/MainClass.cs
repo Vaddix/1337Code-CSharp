8e26bec2e2bd88442670b585e670899aaf8f1f3d
Vaddix <megaman20xx@gmail.com>
2017-05-30 17:04:12 -0500

    Added Hammning Distance problem and starter for Two Numbers problem

---
+++ HammingDistance/HammingDistance/MainClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HammingDistance
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter two numbers: ");
+            string one = Console.ReadLine();
+            int numOne = Convert.ToInt32(one);
+            string two = Console.ReadLine();
+            int numTwo = Convert.ToInt32(two);
+            int dist = new Solution().HammingDistance(numOne, numTwo);
+            Console.WriteLine("The Hamming Distance is: " + dist);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HammingDistance/HammingDistance/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace HammingDistance
+{
+    public class Solution
+    {
+        public int HammingDistance(int x, int y)
+        {
+            int dist = 0;
+            int length = max(x, y);
+            length = log(length);
+            for (int i=0; i < length; i++)
+            {
+                if (x % 2 != y % 2)
+                {
+                    dist++;
+                }
+                x /= 2;
+                y /= 2;
+            }
+            return dist;
+        }
+
+        private int log(int length)
+        {
+            int counter = 0;
+            while (length > 0)
+            {
+                counter++;
+                length /= 2;
+            }
+            return counter;
+        }
+
+        private int max(int one, int two)
+        {
+            if (one < two)
+                return two;
+            return one;
+        }
+    }
+}
